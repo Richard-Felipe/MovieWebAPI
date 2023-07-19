f3cee8472536da1163ca1afd4eeef04dac5f7b1c
Richard Felipe <richardfelipewt@hotmail.com>
2023-07-19 12:42:50 -0300

    Adicionando as pastas para estruturação da aplicação, pacotes de dependência e o primeiro model

---
+++ Models/Movie.cs
@@ -0,0 +1,9 @@
+﻿namespace MovieWebAPI.Models;
+
+public class Movie
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Category { get; set; }
+    public int DurationInMinutes { get; set; }
+}
